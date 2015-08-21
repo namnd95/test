a84e046947150de975b1e88c1a1feebe3f598679
namnd95 <net12k44@gmail.com>
2015-08-21 16:48:17 +0700

    Change compare function output from int to boolean

--- core/compare/functions.py
+++ core/compare/functions.py
@@ -8,7 +8,7 @@ def compareLineIgnoreSpace(line, out, ans, compareFunction):
         return Result(score=0, verdict='Line %d length mismatch' % line)
     
     for i in xrange(len(out)):
-        if compareFunction(out[i], ans[i])==0:
+        if compareFunction(out[i], ans[i]):
             return Result(
                 score=0,
                 verdict='Line %d at %d output %s answer %s' % 
@@ -41,7 +41,7 @@ def compareIgnoreSpace(output, answer, compareFunction):
     return Result(1, 'AC')
     
 def equal(output, answer):
-    return int(output==answer)
+    return output==answer
     
 def equalWithEpsilon(output, answer):
     try:
@@ -50,7 +50,7 @@ def equalWithEpsilon(output, answer):
     except:
         return 0
     
-    return int(abs(out-ans) < 1e-6)
+    return abs(out-ans) < 1e-6
     
 def equalIgnoreSpace(output, answer, *args, **kargs):
     return compareIgnoreSpace(output, answer, equal)
