1e53e966e127c9ad28583adcbadf477f487cad84
namnd95 <net12k44@gmail.com>
2016-02-03 10:00:00 +0700

    Add test for use default config for language

--- tests/test_core_problem.py
+++ tests/test_core_problem.py
@@ -12,6 +12,11 @@ from core.problem.test_case import TestCase
 PATH = os.path.dirname(__file__)
 PATH = os.path.join(PATH, 'test_core_problem')
 
+core.problem.config.default_config = core.utils.from_string(
+    core.problem.config.ProblemConfig,
+    file_name=os.path.join(PATH, 'config.json')
+)
+
 
 class TestTestCase(unittest.TestCase):
 
@@ -181,41 +186,71 @@ class TestProblemConfig(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.default_config = core.utils.from_string(
-            core.problem.config.ProblemConfig,
-            file_name=os.path.join(PATH, 'config.json')
+        cls.config = core.problem.config.ProblemConfig(
+            stdin=False, stdout=False, stop=False,
+            language={
+                "c++": {
+                    'compile': 'g++ -g -std=gnu++11 $file -O2 -o "$problem\"',
+                    "run": "$problem"
+                }
+            }
         )
 
     def setUp(self):
-        self.default_config = TestProblemConfig.default_config
+        self.config = TestProblemConfig.config
 
     def test_get_stdin(self):
-        self.assertTrue(self.default_config.get_stdin())
+        self.assertTrue(core.problem.config.default_config.get_stdin())
+        self.assertFalse(self.config.get_stdin())
 
     def test_get_stdout(self):
-        self.assertFalse(self.default_config.get_stdout())
+        self.assertFalse(core.problem.config.default_config.get_stdout())
+        self.assertFalse(self.config.get_stdout())
 
     def test_get_stop(self):
-        self.assertTrue(self.default_config.can_stop())
+        self.assertTrue(core.problem.config.default_config.can_stop())
+        self.assertFalse(self.config.can_stop())
 
     def test_get_compile_command(self):
         self.assertEqual(
-            self.default_config.get_compile_command('c++'),
+            core.problem.config.default_config.get_compile_command('c++'),
             'g++ -g -std=gnu++11 $file  -o "$problem"'
         )
         self.assertEqual(
-            self.default_config.get_compile_command('pas'),
+            core.problem.config.default_config.get_compile_command('pas'),
+            'fpc -g $file  -o"$problem"'
+        )
+
+        self.assertEqual(
+            self.config.get_compile_command('c++'),
+            'g++ -g -std=gnu++11 $file -O2 -o "$problem"'
+        )
+
+        # Use default config
+        self.assertEqual(
+            self.config.get_compile_command('pas'),
             'fpc -g $file  -o"$problem"'
         )
 
     def test_get_run_command(self):
         self.assertEqual(
-            self.default_config.get_run_command('c++'),
+            core.problem.config.default_config.get_run_command('c++'),
             '$problem'
         )
 
         self.assertEqual(
-            self.default_config.get_run_command('pas'),
+            core.problem.config.default_config.get_run_command('pas'),
+            './$problem'
+        )
+
+        self.assertEqual(
+            self.config.get_run_command('c++'),
+            '$problem'
+        )
+
+        # Use default config
+        self.assertEqual(
+            self.config.get_run_command('pas'),
             './$problem'
         )
 
