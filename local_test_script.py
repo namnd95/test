033f95991dbc429489670b2f6c313c478dd40821
namnd95 <net12k44@gmail.com>
2016-02-19 21:26:55 +0700

    Check PEP8

--- local_test_script.py
+++ local_test_script.py
@@ -1,16 +1,17 @@
 import core
 import core.utils
 
+
 class ProblemSubmission:
-    
-    def __init__(self, problem, language, file):        
-        self.problem = core.Problem(**problem)        
-        
+
+    def __init__(self, problem, language, file):
+        self.problem = core.Problem(**problem)
+
         result = core.make_submission(self.problem, language, file)
         print result.get_test_case_results()
 
 
 prob_sub = core.utils.from_string(
     ProblemSubmission,
-    file_name = 'local_test.json'
+    file_name='local_test.json'
 )
